a36e01487482a6a735625e1e4a9c7264884636c0
xiaosongfu <xiaosong.fu@outlook.com>
2019-12-20 18:05:08 +0800

    recursive call for struct

--- fvpp.go
+++ fvpp.go
@@ -23,24 +23,44 @@ func Println(value interface{}) {
 }
 
 // CustomFormat is used for customize the format、 separator and wrapper with what you like
+// if value's type is builtin type,return ...
+// if value's type is struct,
 func CustomFormat(value interface{}, fieldFormat, fieldSeparator, wrapperBegin, wrapperEnd string) string {
-	var tmp []string
-
 	rType := reflect.TypeOf(value)
 	rValue := reflect.ValueOf(value)
 
-	n := rType.NumField()
-	for i := 0; i < n; i++ {
-		f := fmt.Sprintf("%v", rType.Field(i).Name)
-		v := fmt.Sprintf("%v", rValue.Field(i).Interface())
-		t := fmt.Sprintf("%v", rType.Field(i).Type)
+	if rType.Kind() == reflect.Struct { // struct type
+		var tmp []string
+		n := rType.NumField()
 
-		format := fieldFormat
-		format = strings.ReplaceAll(format, Field, f)
-		format = strings.ReplaceAll(format, Value, v)
-		format = strings.ReplaceAll(format, Type, t)
+		for i := 0; i < n; i++ {
+			// first,we assume the field is builtin type,so set it `rValue.Field(i).Interface()`
+			// but if it is a struct,we need recursive each field to got it's value use current function
+			fieldValue := rValue.Field(i).Interface()
 
-		tmp = append(tmp, format)
+			if rType.Field(i).Type.Kind() == reflect.Struct {
+				fieldValue = CustomFormat(rValue.Field(i).Interface(), fieldFormat, fieldSeparator, wrapperBegin, wrapperEnd)
+			}
+
+			f := fmt.Sprintf("%v", rType.Field(i).Name)
+			v := fmt.Sprintf("%v", fieldValue)
+			t := fmt.Sprintf("%v", rType.Field(i).Type)
+
+			format := fieldFormat
+			format = strings.ReplaceAll(format, Field, f)
+			format = strings.ReplaceAll(format, Value, v)
+			format = strings.ReplaceAll(format, Type, t)
+
+			tmp = append(tmp, format)
+		}
+		return assembleResult(strings.Join(tmp, fieldSeparator), wrapperBegin, wrapperEnd)
+	} else { // builtin types
+		//content :=
+		return assembleResult(fmt.Sprintf("%v", value), wrapperBegin, wrapperEnd)
 	}
-	return fmt.Sprintf("%s%s%s", wrapperBegin, strings.Join(tmp, fieldSeparator), wrapperEnd)
+}
+
+// assembleResult assemble result
+func assembleResult(content, wrapperBegin, wrapperEnd string) string {
+	return fmt.Sprintf("%s%s%s", wrapperBegin, content, wrapperEnd)
 }
--- fvpp_test.go
+++ fvpp_test.go
@@ -1,15 +1,28 @@
 package fvpp
 
 import (
-	"fmt"
 	"testing"
 )
+
+type car struct {
+	Brand    string
+	Wheel    int
+	Price    float64
+	Scrapped bool
+}
 
 type user struct {
 	Name    string
 	Married bool
 	Cars    int
 	Deposit float64
+	Car     car
+}
+
+type people struct {
+	Face string
+	Eye  string
+	User user
 }
 
 var u = user{
@@ -17,58 +30,55 @@ var u = user{
 	Married: true,
 	Cars:    2,
 	Deposit: 200000,
+	Car:     c,
+}
+
+var c = car{
+	Brand:    "BMW",
+	Wheel:    4,
+	Price:    280000,
+	Scrapped: false,
+}
+
+var p = people{
+	Face: "black",
+	Eye:  "blue",
+	User: u,
 }
 
 func TestPretty(t *testing.T) {
+	//t.Run("BuiltinType", testBuiltinType)
+	//t.Run("StructType", testStructType)
+	t.Run("NestedStructType", testNestedStructType)
+	//t.Run("MultilayerNestedStructType", testMultilayerNestedStructType)
+}
+
+func testMultilayerNestedStructType(t *testing.T) {
 	tests := []struct {
-		user
-		want string
+		people people
+		want   string
 	}{
 		{
-			user: u,
-			want: "[Name:'bili'(string) Married:'true'(bool) Cars:'2'(int) Deposit:'200000'(float64)]",
+			people: p,
+			want:   "",
 		},
 		{
-			user: user{
-				Name:    "kili",
-				Married: false,
-				Cars:    0,
-				Deposit: 20000,
+			people: people{
+				Face: "yellow",
+				Eye:  "yellow",
+				User: user{
+					Name:    "vik",
+					Married: false,
+					Cars:    0,
+					Deposit: 10000,
+					Car: car{
+						Brand:    "XiaoNiu",
+						Wheel:    2,
+						Price:    6000,
+						Scrapped: false,
+					},
+				},
 			},
-			want: "[Name:'kili'(string) Married:'false'(bool) Cars:'0'(int) Deposit:'20000'(float64)]",
-		},
-	}
-
-	for _, tt := range tests {
-		got := Pretty(tt.user)
-		if tt.want != got {
-			t.Errorf("test failed, want: %s, but got: %s", tt.want, got)
-		}
-	}
-}
-
-func TestCustomFormat(t *testing.T) {
-	t.Run("BuiltinType", testBuiltinType)
-	t.Run("StructType", testStructType)
-}
-
-func testStructType(t *testing.T) {
-	tests := []struct {
-		Face string
-		Eye  string
-		User user
-		want string
-	}{
-		{
-			Face: "yellow",
-			Eye:  "yellow",
-			User: u,
-			want: "",
-		},
-		{
-			Face: "black",
-			Eye:  "blue",
-			User: u,
 			want: "",
 		},
 	}
@@ -81,41 +91,130 @@ func testStructType(t *testing.T) {
 	}
 }
 
-func testBuiltinType(t *testing.T) {
+func testNestedStructType(t *testing.T) {
 	tests := []struct {
-		fieldFormat    string
-		fieldSeparator string
-		wrapperBegin   string
-		wrapperEnd     string
-		want           string
+		user user
+		want string
 	}{
 		{
-			fieldFormat:    fmt.Sprintf("%s-%s-%s", Field, Type, Value),
-			fieldSeparator: ";",
-			wrapperBegin:   "<",
-			wrapperEnd:     ">",
-			want:           "<Name-string-bili;Married-bool-true;Cars-int-2;Deposit-float64-200000>",
+			user: u,
+			want: "",
 		},
 		{
-			fieldFormat:    fmt.Sprintf("%s>%s>%s", Field, Value, Type),
-			fieldSeparator: "|",
-			wrapperBegin:   "(",
-			wrapperEnd:     ")",
-			want:           "(Name>bili>string|Married>true>bool|Cars>2>int|Deposit>200000>float64)",
-		},
-		{
-			fieldFormat:    fmt.Sprintf("%s#%s#%s", Type, Field, Value),
-			fieldSeparator: " ",
-			wrapperBegin:   "{",
-			wrapperEnd:     "}",
-			want:           "{string#Name#bili bool#Married#true int#Cars#2 float64#Deposit#200000}",
+			user: user{
+				Name:    "bill",
+				Married: true,
+				Cars:    100,
+				Deposit: 100000000,
+				Car: car{
+					Brand:    "Masaladi",
+					Wheel:    6,
+					Price:    10000000,
+					Scrapped: false,
+				},
+			},
+			want: "",
 		},
 	}
 
 	for _, tt := range tests {
-		got := CustomFormat(u, tt.fieldFormat, tt.fieldSeparator, tt.wrapperBegin, tt.wrapperEnd)
+		got := Pretty(tt.user)
+		if got != tt.want {
+			t.Errorf("test failed, want: %s, but got: %s", tt.want, got)
+		}
+	}
+}
+
+func testStructType(t *testing.T) {
+	tests := []struct {
+		car
+		want string
+	}{
+		{
+			car:  c,
+			want: "[Brand:'BMW'(string) Wheel:'4'(int) Price:'280000'(float64) Scrapped:'false'(bool)]",
+		},
+		{
+			car: car{
+				Brand:    "Audi",
+				Wheel:    2,
+				Price:    100000,
+				Scrapped: false,
+			},
+			want: "[Brand:'Audi'(string) Wheel:'2'(int) Price:'100000'(float64) Scrapped:'false'(bool)]",
+		},
+	}
+
+	for _, tt := range tests {
+		got := Pretty(tt.car)
 		if tt.want != got {
 			t.Errorf("test failed, want: %s, but got: %s", tt.want, got)
 		}
 	}
 }
+
+func testBuiltinType(t *testing.T) {
+	intValue := 2
+	strValue := 3
+	mapValue := map[string]int{
+		"key1": 1,
+		"key2": 2,
+	}
+
+	intWant := "[field:'2'(int)]"
+	strWant := "[]"
+	mapWant := ""
+
+	got := ""
+	if got = Pretty(intValue); got != intWant {
+		t.Errorf("test failed, want: %s, but got: %s", intWant, got)
+	}
+	if got = Pretty(strValue); got != strWant {
+		t.Errorf("test failed, want: %s, but got: %s", strWant, got)
+	}
+	if got = Pretty(mapValue); got != mapWant {
+		t.Errorf("test failed, want: %s, but got: %s", mapWant, got)
+	}
+}
+
+//func TestCustomFormat(t *testing.T) {
+//}
+//
+//func testStructTypeCustomFormat(t *testing.T) {
+//	tests := []struct {
+//		fieldFormat    string
+//		fieldSeparator string
+//		wrapperBegin   string
+//		wrapperEnd     string
+//		want           string
+//	}{
+//		{
+//			fieldFormat:    fmt.Sprintf("%s-%s-%s", Field, Type, Value),
+//			fieldSeparator: ";",
+//			wrapperBegin:   "<",
+//			wrapperEnd:     ">",
+//			want:           "",
+//		},
+//		{
+//			fieldFormat:    fmt.Sprintf("%s>%s>%s", Field, Value, Type),
+//			fieldSeparator: "|",
+//			wrapperBegin:   "(",
+//			wrapperEnd:     ")",
+//			want:           "",
+//		},
+//		{
+//			fieldFormat:    fmt.Sprintf("%s#%s#%s", Type, Field, Value),
+//			fieldSeparator: " ",
+//			wrapperBegin:   "{",
+//			wrapperEnd:     "}",
+//			want:           "",
+//		},
+//	}
+//
+//	for _, tt := range tests {
+//		got := CustomFormat(c, tt.fieldFormat, tt.fieldSeparator, tt.wrapperBegin, tt.wrapperEnd)
+//		if tt.want != got {
+//			t.Errorf("test failed, want: %s, but got: %s", tt.want, got)
+//		}
+//	}
+//}
